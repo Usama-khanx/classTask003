4080c3d725bae772d21e8dad30a323ebfc502452
Usama-khanx <usamak2004@gmail.com>
2024-09-01 10:43:08 +0500

    created a separated file for js

--- script/script.js
+++ script/script.js
@@ -3,3 +3,30 @@
 // // if(question_array === "peshawar"){
 
 // // }
+
+
+function incorrect(usama){
+    alert("Sorry incorred try Next time")
+    usama.style.backgroundColor = 'red';
+}
+function Correct(usama){
+    alert("Correct Answer")
+    usama.style.backgroundColor = 'green';
+}
+
+
+
+function scoring(){
+   
+
+    var totalScore = document.getElementsByClassName('total')
+
+    var que_1 = document.getElementById('q-1')
+    var correctAnswer = document.getElementById('isl');
+    if(que_1 === correctAnswer){
+        total_score = total_score + 10;
+        document.write(totalScore)
+    }else{
+        
+    }
+}
